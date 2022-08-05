bc743d567afebd8190044b661e3aa039dc162605
KarenCCEO <karen.suarez@cceo.com.mx>
2022-08-05 15:00:46 -0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <Navbar/>
+  <div class="continer"></div>
+  <router-view/>
+</template>
+
+
+<script>
+import  Navbar  from './components/Navbar.vue'
+
+export default {
+  components:{
+    Navbar
+  }
+}
+</script>
+
+
+<style>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,28 @@
+<template>
+ <form >
+  <input type="text"
+  class="from-control my-2"
+  placeholder="Ingrese su nombre"
+  v-model="texto"
+  >
+ </form>
+ <p>
+  {{texto}}
+ </p>
+</template>
+
+<script>
+
+
+export default {
+  name: 'Home',
+  components: {
+ 
+},
+data(){
+  return{
+    texto:''
+  }
+}
+}
+</script>
