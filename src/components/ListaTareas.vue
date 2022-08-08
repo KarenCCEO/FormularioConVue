fa9fab0524566b8d7a63de231d9995cef25e8040
KarenCCEO <karen.suarez@cceo.com.mx>
2022-08-08 12:19:42 -0500

    usando un v-for dentro de otro v-for

--- src/components/ListaTareas.vue
+++ src/components/ListaTareas.vue
@@ -12,10 +12,20 @@
     </tr>
   </thead>
   <tbody>
+    <!--Se imprimen las tareas segun el id random que genera vue-->
     <tr v-for="item in tareas" :key="item.id">
       <th scope="row">{{item.id}}</th>
       <td>{{item.nombre}}</td>
-      <td>{{item.categorias}}</td>
+      <td>
+        <span v-for="(cat, index) in item.categorias" :key="index">
+    
+        {{
+            //separando con comas los datos que hay dentro dle array categorias
+            (item.categorias.lenght === index + 1) ? cat : cat + ', '
+        }}
+
+        </span>
+        </td>
       <td>{{item.estado}}</td>
       <td>{{item.numero}}</td>
       <td>esta es una accion</td>
