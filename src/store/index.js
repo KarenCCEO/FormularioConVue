1a433c7d4292375d294701e98a978fb3a2682f38
KarenCCEO <karen.suarez@cceo.com.mx>
2022-08-08 14:01:47 -0500

    formulario y rutas para editar e l item

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,5 @@
 import { createStore } from 'vuex'
+import router from '../router'
 
 export default createStore({
   state: {
@@ -21,11 +22,22 @@ export default createStore({
     },
     eliminar(state, payload){
       //filtramos todos los elementos que son diferentes de el id que esta en payload
+      
       state.tareas =state.tareas.filter(item => item.id !== payload)
     },
     tarea(state, payload){
+      if(!state.tareas.find(item => item.id === payload )){
+        router.push('/')
+        return
+      }
       state.tarea = state.tareas.find(item => item.id === payload )
 
+    },
+    update(state, payload){
+      //la funcion map nos regresa un dato dependiendo de la condicion que nosotros le demos
+      //por lo tanto esta funcion devuelve el array modificado 
+      state.tareas = state.tareas.map(item => item.id === payload.id ? payload : item )
+      router.push('/')
     }
 
   },
@@ -42,6 +54,9 @@ export default createStore({
     //imprime las tareas en la pagina de editar 
     setTarea({commit}, id){
       commit('tarea', id)
+    },
+    updateTarea({commit}, tarea){
+      commit('update', tarea)
     }
   },
   modules: {
--- src/views/EditarC.vue
+++ src/views/EditarC.vue
@@ -1,21 +1,28 @@
 <template>
 Editar{{$route.params.id}} - {{tarea}}
+<form @submit.prevent="updateTarea(tarea)">
+    <InputC :tarea="tarea"></InputC>
+</form>
+
   
 </template>
 
 <script>
 import {mapState, mapActions} from 'vuex'
-export default {
-    computed:{
-        ...mapState(['tarea'])
-    },
-    methods:{
-        ...mapActions(['setTarea'])
-    },
-    created(){
-        this.setTarea(this.$route.params.id)
-    }
 
+import InputC from '@/components/InputC.vue'
+export default {
+    components: { InputC },
+    computed: {
+        ...mapState(["tarea"])
+    },
+    methods: {
+        ...mapActions(["setTarea", "updateTarea"])
+    },
+    created() {
+        this.setTarea(this.$route.params.id);
+    }
+    
 }
 </script>
 
