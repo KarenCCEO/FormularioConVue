a241abd92b5830bf7350bbe4f13c18687e474f47
KarenCCEO <karen.suarez@cceo.com.mx>
2022-08-08 10:06:55 -0500

    v-model para check box y number

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -26,6 +26,62 @@
     >
     <label for="check-2" class="form-check-label">Node.js</label>
   </div>
+  <div class="mt-2">
+    <div class="form-check form-check-inline">
+      <input
+      type="radio"
+      id="radio-1"
+      class="form-check-input"
+      value="URGENTE"
+      v-model="tarea.estado"
+      >
+      
+      <label 
+      for="radio-1"
+      class="form-check-label"
+      > URGENTE</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input
+      type="radio"
+      id="radio-2"
+      class="form-check-input"
+      value="relax"
+       v-model="tarea.estado"
+      >
+      
+      <label 
+      for="radio-2"
+      class="form-check-label"
+      > relax</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input
+      type="radio"
+      id="radio-3"
+      class="form-check-input"
+      value="No Importante"
+       v-model="tarea.estado"
+      >
+      
+      <label 
+      for="radio-3"
+      class="form-check-label"
+      >No importante</label>
+    </div>
+
+  </div>
+
+  <div class="mt-2">
+   <input
+   type="number"
+   class="form-control"
+   v-model.number="tarea.numero">
+  
+    
+
+  </div>
+
  </form>
  <hr>
  <p>
@@ -45,7 +101,9 @@ data(){
   return{
     tarea:{
       nombre:'',
-      categorias:[]
+      categorias:[],
+      estado:'',
+      numero:0
 
     }
   
