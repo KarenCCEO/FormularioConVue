c241c9237738a8056fe49f696b15cb30dfd06c76
KarenCCEO <karen.suarez@cceo.com.mx>
2022-08-08 11:19:50 -0500

    Separando inputs en componentes

---
+++ src/components/InputC.vue
@@ -0,0 +1,110 @@
+<template>
+<!--Se agrega el .prevent para evitar que se refresque la pagina por accion del get -->
+
+  <input 
+  type="text"
+  class="from-control my-2"
+  placeholder="Ingrese su nombre"
+  v-model="tarea.nombre"
+  >
+  <div class="form-check form-check-inline">
+    <input 
+    type="checkbox"
+    id="check-1"
+    class="form-check-input"
+    v-model="tarea.categorias"
+    value="JavaScript"
+    >
+    <label for="check-1" class="form-check-label">JavaScript</label>
+  </div>
+  <div class="form-check form-check-inline">
+    <input 
+    type="checkbox"
+    id="check-2"
+    class="form-check-input"
+    v-model="tarea.categorias"
+    value="node js"
+    >
+    <label for="check-2" class="form-check-label">Node.js</label>
+  </div>
+  <div class="mt-2">
+    <div class="form-check form-check-inline">
+      <input
+      type="radio"
+      id="radio-1"
+      class="form-check-input"
+      value="URGENTE"
+      v-model="tarea.estado"
+      >
+      
+      <label 
+      for="radio-1"
+      class="form-check-label"
+      > URGENTE</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input
+      type="radio"
+      id="radio-2"
+      class="form-check-input"
+      value="relax"
+       v-model="tarea.estado"
+      >
+      
+      <label 
+      for="radio-2"
+      class="form-check-label"
+      > relax</label>
+    </div>
+    <div class="form-check form-check-inline">
+      <input
+      type="radio"
+      id="radio-3"
+      class="form-check-input"
+      value="No Importante"
+       v-model="tarea.estado"
+      >
+      
+      <label 
+      for="radio-3"
+      class="form-check-label"
+      >No importante</label>
+    </div>
+
+  </div>
+
+  <div class="mt-2">
+   <input
+   type="number"
+   class="form-control"
+   v-model.trim="tarea.numero">
+  
+    
+
+  </div>
+  <div class="d-grid gap-2">
+
+  <button class="btn btn-dark mt-2" type="submit" :disabled="bloquear">procesar</button>
+</div>
+  
+
+
+  
+</template>
+
+<script>
+export default {
+    props:{
+        tarea: Object
+
+    },
+    computed:{
+  bloquear(){
+    return this.tarea.nombre.trim() === "" ? true : false
+  }
+
+}
+
+}
+</script>
+
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,94 +1,11 @@
 <template>
-<!--Se agrega el .prevent para evitar que se refresque la pagina por accion del get -->
- <form @submit.prevent="procesarFormulario" >
-  <input 
-  type="text"
-  class="from-control my-2"
-  placeholder="Ingrese su nombre"
-  v-model="tarea.nombre"
-  >
-  <div class="form-check form-check-inline">
-    <input 
-    type="checkbox"
-    id="check-1"
-    class="form-check-input"
-    v-model="tarea.categorias"
-    value="JavaScript"
-    >
-    <label for="check-1" class="form-check-label">JavaScript</label>
-  </div>
-  <div class="form-check form-check-inline">
-    <input 
-    type="checkbox"
-    id="check-2"
-    class="form-check-input"
-    v-model="tarea.categorias"
-    value="node js"
-    >
-    <label for="check-2" class="form-check-label">Node.js</label>
-  </div>
-  <div class="mt-2">
-    <div class="form-check form-check-inline">
-      <input
-      type="radio"
-      id="radio-1"
-      class="form-check-input"
-      value="URGENTE"
-      v-model="tarea.estado"
-      >
-      
-      <label 
-      for="radio-1"
-      class="form-check-label"
-      > URGENTE</label>
-    </div>
-    <div class="form-check form-check-inline">
-      <input
-      type="radio"
-      id="radio-2"
-      class="form-check-input"
-      value="relax"
-       v-model="tarea.estado"
-      >
-      
-      <label 
-      for="radio-2"
-      class="form-check-label"
-      > relax</label>
-    </div>
-    <div class="form-check form-check-inline">
-      <input
-      type="radio"
-      id="radio-3"
-      class="form-check-input"
-      value="No Importante"
-       v-model="tarea.estado"
-      >
-      
-      <label 
-      for="radio-3"
-      class="form-check-label"
-      >No importante</label>
-    </div>
+ <h1 class="my-5">Formulario con VUE </h1>
+ <form @submit.prevent="procesarFormulario">
 
-  </div>
-
-  <div class="mt-2">
-   <input
-   type="number"
-   class="form-control"
-   v-model.trim="tarea.numero">
-  
-    
-
-  </div>
-  <div class="d-grid gap-2">
-
-  <button class="btn btn-dark mt-2" type="submit" :disabled="bloquear">procesar</button>
-</div>
-  
+ <inputC :tarea="tarea"/>
 
  </form>
+
  <hr>
  <p>
   {{tarea}}
@@ -96,7 +13,8 @@
 </template>
 
 <script>
-import { computed } from '@vue/runtime-core'
+import inputC from '../components/InputC.vue'
+
 
 
 
@@ -104,11 +22,13 @@ import { computed } from '@vue/runtime-core'
 export default {
   name: 'Home',
   components: {
+    inputC
  
 },
 data(){
   return{
     tarea:{
+      id:'',
       nombre:'',
       categorias:[],
       estado:'',
@@ -137,12 +57,7 @@ methods:{
     }
   }
 
-},
-computed:{
-  bloquear(){
-    return this.tarea.nombre.trim() === "" ? true : false
-  }
-
 }
+
 }
 </script>
